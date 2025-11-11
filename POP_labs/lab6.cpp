e4acfa5f186a9db91902d21a2eb99361b20d6b68
tobeyboy1 <166401805+tobeyboy1@users.noreply.github.com>
2025-11-11 12:08:14 +0700

    Сдал лабу 6

--- POP_labs/lab6.cpp
+++ POP_labs/lab6.cpp
@@ -5,11 +5,6 @@
 #define CLASS_NAME "F(DSHNLK#NROUFYE(*nlk234nouo3yf98h23n"
 #define MAX_IDS IDS_STRING107+16
 
-struct UserData {
-    HBRUSH hBrh1;
-    HBRUSH hBrh2;
-};
-
 LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(
@@ -24,11 +19,10 @@ int WINAPI WinMain(
     wc.lpfnWndProc = MainWndProc;
     wc.hInstance = hInstance;
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     wc.style = CS_HREDRAW | CS_VREDRAW;
     wc.lpszClassName = CLASS_NAME;
 
-    RegisterClass(&wc);
+    if (!RegisterClass(&wc)) return -1;
 
     int screenWidth = GetSystemMetrics(SM_CXSCREEN);
     int screenHeight = GetSystemMetrics(SM_CYSCREEN);
@@ -36,19 +30,16 @@ int WINAPI WinMain(
     int indentX = (int)(screenWidth * 0.07);  // 7% от ширины экрана
     int indentY = (int)(screenHeight * 0.07); // 7% от высоты экрана
 
-    UserData userData = { 0 };
-
     HWND hWnd = CreateWindowEx(
         0,
         CLASS_NAME,
         "Lab6",
         WS_OVERLAPPEDWINDOW,
-        indentX, 
-        indentY, 
+        indentX,
+        indentY,
         screenWidth - 2 * indentX,
         screenHeight - 2 * indentY,
-        NULL, NULL, hInstance, &userData
-    );
+        NULL, NULL, hInstance, NULL);
 
     if (hWnd == NULL) return 0;
 
@@ -68,42 +59,56 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) 
     switch (msg) {
 
     case WM_CREATE: {
-        CREATESTRUCT* pCreate = (CREATESTRUCT*)(lParam);
-        UserData* pUserData = (UserData*)(pCreate->lpCreateParams);
-
+        
         HINSTANCE hInstance = (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE);
 
         HBITMAP hBMP1 = LoadBitmap(hInstance, MAKEINTRESOURCE(IDB_BITMAP1));
-        HBITMAP hBMP2 = LoadBitmap(hInstance, MAKEINTRESOURCE(IDB_BITMAP2));
 
-        pUserData->hBrh1 = CreatePatternBrush(hBMP1);
-        pUserData->hBrh2 = CreatePatternBrush(hBMP2);
+        HBRUSH hBr1 = CreatePatternBrush(hBMP1);
+        DeleteObject(hBMP1); //load объекты можно не удалять
 
-        SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)pUserData);
-
-        SetClassLongPtr(hWnd, GCLP_HBRBACKGROUND, (LONG)pUserData->hBrh1);
-
-        DeleteObject(hBMP1);
-        DeleteObject(hBMP2);
+        SetClassLongPtr(hWnd, GCLP_HBRBACKGROUND, (LONG_PTR)hBr1);
         return 0;
     }
     case WM_COMMAND: {
-        UserData* pUserData = (UserData*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
 
         switch (LOWORD(wParam)) {
 
         case ID_BITMAP1: {
-            SetClassLongPtr(hWnd, GCLP_HBRBACKGROUND, (LONG)pUserData->hBrh1);
+            HINSTANCE hInstance = (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE); //можно было обернуть в ф-ю 
+
+            HBITMAP hBMP1 = LoadBitmap(hInstance, MAKEINTRESOURCE(IDB_BITMAP1));
+
+            HBRUSH hBr1 = CreatePatternBrush(hBMP1);
+            DeleteObject(hBMP1);
+
+            HBRUSH hOldBrush = (HBRUSH)GetClassLongPtr(hWnd, GCLP_HBRBACKGROUND);
+
+            SetClassLongPtr(hWnd, GCLP_HBRBACKGROUND, (LONG_PTR)hBr1); //возвращает старую кисть
+            DeleteObject(hOldBrush); //можно передать сразу SetClassLongPtr
+
             InvalidateRect(hWnd, NULL, TRUE);
             break;
         }
+
         case ID_BITMAP2: {
-            SetClassLongPtr(hWnd, GCLP_HBRBACKGROUND, (LONG)pUserData->hBrh2);
+            HINSTANCE hInstance = (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE);
+
+            HBITMAP hBMP2 = LoadBitmap(hInstance, MAKEINTRESOURCE(IDB_BITMAP2));
+
+            HBRUSH hBr2 = CreatePatternBrush(hBMP2);
+            DeleteObject(hBMP2);
+
+            HBRUSH hOldBrush = (HBRUSH)GetClassLongPtr(hWnd, GCLP_HBRBACKGROUND);
+
+            SetClassLongPtr(hWnd, GCLP_HBRBACKGROUND, (LONG_PTR)hBr2);
+            DeleteObject(hOldBrush);
+
             InvalidateRect(hWnd, NULL, TRUE);
             break;
         }
         default: {
-            if (LOWORD(wParam) >= IDS_STRING107 && LOWORD(wParam) <= MAX_IDS) {
+            if (LOWORD(wParam) >= IDS_STRING107 && LOWORD(wParam) < MAX_IDS) {
                 HINSTANCE hInstance = (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE);
                 TCHAR sz[160];
                 LoadString(hInstance, LOWORD(wParam), sz, sizeof(sz) / sizeof(TCHAR));
@@ -150,10 +155,7 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) 
     }
 
     case WM_DESTROY: {
-        UserData* pUserData = (UserData*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
 
-        DeleteObject(pUserData->hBrh1);
-        DeleteObject(pUserData->hBrh2);
         PostQuitMessage(0);
         return 0;
     }
